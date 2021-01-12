a8157172c7265a17ca18767f259d29b16a60d95f
Theophilus Omoregbee <theo4u@ymail.com>
2021-01-12 16:54:47 -0500

    feat: send message to change body color from content script and reply with response

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,9 @@
 console.log('script is loaded')
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if(message.action === 'change-bg-color'){
+        document.body.style.backgroundColor = message.color;
+        sendResponse('Done changing bg color of page')
+        return true
+    }
+});
--- popup.js
+++ popup.js
@@ -5,11 +5,11 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+changeColor.onclick = function (element) {
+    const color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {action: "change-bg-color", color}, function(response) {
+            console.log('response is:', response)
+        });  
     });
-  };
+  }
